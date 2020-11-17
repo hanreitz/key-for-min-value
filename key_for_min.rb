ef989f255eb58bb4a52dde07d59e12f936ae7831
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2020-11-17 11:06:30 -0500

    commit tree 65e859d9a9a5f518cc63664fb01518859de50166

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    current_lowest = nil
+    current_key = nil
+    name_hash.each do |name, value|
+        if current_lowest == nil || value < current_lowest
+            current_lowest = value
+            current_key = name
+        end
+    end
+    current_key
 end
